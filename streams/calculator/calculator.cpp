f71f0c6d2a9a3a9f8060397e70866d73077186dd
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2026-02-12 13:38:33 -0600

    starter code for stream calculator

---
+++ streams/calculator/calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+double calculate(std::string line) {
+  double first_number = 0, second_number = 0;
+  char op = ' ';
+
+  std::istringstream iss(line);
+  // TODO(@Student): extract <first_number> <op> <second_number> from iss
+
+  double result = 0;
+  // TODO(@Student): compute result based on op symbol,
+  //                 allowed symbols are +, -, *, /
+
+  return result;
+}
+
+int main() {
+  std::cout << "Enter the expression to calculate in one line: " << std::endl;
+  std::string line;
+  std::getline(std::cin, line);
+
+  try {
+    std::cout << "result: " << calculate(line) << std::endl;
+  } catch (const std::invalid_argument& err) {
+    std::cout << err.what() << std::endl;
+  }
+
+  return 0;
+}
