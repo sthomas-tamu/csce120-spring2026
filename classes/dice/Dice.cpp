1a2175cc0de53dd3a209e12878ebc3a76ba56103
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2026-02-26 14:00:00 -0600

    starter code for a dice class

---
+++ classes/dice/Dice.cpp
@@ -0,0 +1,4 @@
+#include "Dice.h"
+
+// TODO(@Student) Implement any function prototypes from Dice.h
+
---
+++ classes/dice/Dice.h
@@ -0,0 +1,16 @@
+#ifndef DICE_H_
+#define DICE_H_
+
+// TODO(@Student) Create a Dice class
+//
+//   Should know how many sides it has and its current value
+//    - Cannot have a negative number of sides or a negative value
+//    - Should be able to construct a Dice object given how many sides
+//    - Don't allow direct access to how many sides or its current value
+//
+//   Should provide the following functions:
+//    - sides(): reports the number of sides, does not change data
+//    - value(): reports the current value, does not change data
+//    - roll(): rolls the dice to get a new random current value
+
+#endif  // DICE_H_
